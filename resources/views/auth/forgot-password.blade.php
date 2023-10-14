f4f6b06549c5fdae0d591bc9dfba3b8393c28f8c
Gilberto <josegilbertoguzmangutierrez@gmail.com>
2023-10-14 14:21:45 -0600

    traduccion español

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,6 +1,6 @@
 <x-guest-layout>
     <div class="mb-4 text-sm text-gray-600 dark:text-gray-400">
-        {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+        {{ __('¿Olvidaste tu contraseña? Ningún problema. Simplemente háganos saber su dirección de correo electrónico y le enviaremos un enlace para restablecer su contraseña que le permitirá elegir una nueva.') }}
     </div>
 
     <!-- Session Status -->
@@ -12,14 +12,22 @@
         <!-- Email Address -->
         <div>
             <x-input-label for="email" :value="__('Email')" />
-            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required autofocus />
+            <x-text-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')" required
+                autofocus />
             <x-input-error :messages="$errors->get('email')" class="mt-2" />
         </div>
 
-        <div class="flex items-center justify-end mt-4">
-            <x-primary-button>
-                {{ __('Email Password Reset Link') }}
-            </x-primary-button>
+        <div class="flex justify-between my-5">
+            <x-link :href="route('login')">
+                Iniciar Sesión
+            </x-link>
+
+            <x-link :href="route('register')">
+                Crear Cuenta
+            </x-link>
         </div>
+        <x-primary-button>
+            {{ __('Restablecer contraseña de correo electrónico') }}
+        </x-primary-button>
     </form>
 </x-guest-layout>
