ded20e8a2c12b1539af900e65fe6dd09ab6e40c2
Gilberto <josegilbertoguzmangutierrez@gmail.com>
2023-10-23 01:34:00 -0600

    actualizacion de mostrar vacante

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -16,11 +16,25 @@
         </p>
 
         <p class="font-bold text-sm uppercase text-gray-800 my-3">Categoría:
-            <span class="normal-case font-normal">{{ $vacante->categoria_id }}</span>
+            {{-- <span class="normal-case font-normal">{{ $vacante->categoria_id }}</span> --}}
+            <span class="normal-case font-normal">{{ $vacante->categoria->categoria }}</span>
         </p>
 
         <p class="font-bold text-sm uppercase text-gray-800 my-3">Salario:
-            <span class="normal-case font-normal">{{ $vacante->salario_id }}</span>
+            {{-- <span class="normal-case font-normal">{{ $vacante->salario_id }}</span> --}}
+            <span class="normal-case font-normal">{{ $vacante->salario->salario }}</span>
         </p>
+    </div>
+
+    <div class="md:grid md:grid-cols-6 gap-4">
+        <div class="md:col-span-2">
+            <img src="{{ asset('storage/vacantes/' . $vacante->imagen) }}"
+                alt="{{ 'Imagen vacante ' . $vacante->titulo }}">
+        </div>
+
+        <div class="md:col-span-4">
+            <h2 class="text-2xl font-bold mb-5">Descripción del Puesto</h2>
+            <p>{{ $vacante->descripcion }}</p>
+        </div>
     </div>
 </div>
