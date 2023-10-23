7710769dbb508231f222ab3662c83501562f182f
Gilberto <josegilbertoguzmangutierrez@gmail.com>
2023-10-23 14:51:11 -0600

    actualizacion y correcion de errores

--- resources/views/livewire/mostrar-vacante.blade.php
+++ resources/views/livewire/mostrar-vacante.blade.php
@@ -37,4 +37,15 @@
             <p>{{ $vacante->descripcion }}</p>
         </div>
     </div>
+
+    @guest
+        <div class="mt-5 bg-gray-50 border-dashed p-5 text-center">
+            <p>
+                ¿Deseas aplicar a esta vacante? <a class="font-bold text-indigo-600" href="{{ route('register') }}">Obten
+                    una cuenta y a plica a esta y
+                    otras vacantes</a>
+            </p>
+        </div>
+    @endguest
+
 </div>
